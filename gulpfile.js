eb032738d41574c45f7e97fe19b7540f9baa6e96
tocology <hwangjun7777@gmail.com>
2016-01-10 13:06:45 +0900

    build(feat): Add ts-lint task

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,11 @@
 var gulp = require('gulp');
+var tslint = require('gulp-tslint');
 
-gulp.task('default', function(){
-  console.log('Hello Gulp');
+gulp.task('default', ['lint']);
+
+gulp.task('lint', function(){
+  return gulp.src([
+    './source/ts/**/**.ts', './test/**/**.test.ts'
+  ]).pipe(tslint())
+    .pipe(tslint.report('verbose'));
 });
