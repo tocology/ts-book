b74e70bc6fccf8214ff08da6bc4b0ae072673522
tocology <hwangjun7777@gmail.com>
2016-02-02 12:15:24 +0900

    feat: Add tsc task

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,27 @@
-var gulp = require('gulp');
-var tslint = require('gulp-tslint');
+var gulp        = require('gulp');
+var tslint      = require('gulp-tslint');
+var ts          = require('gulp-typescript');
+
+// options reference: https://www.npmjs.com/package/gulp-typescript
+var tsProject = ts.createProject({
+  removeComments: true,
+  noImplicitAny: true,
+  target: 'ES3',
+  module: 'commonjs',
+  declarations: false
+})
 
 gulp.task('lint', function(){
   return gulp.src([
     './source/ts/**/**.ts', './test/**/**.test.ts'
   ]).pipe(tslint())
     .pipe(tslint.report('verbose'));
+});
+
+gulp.task('tsc', function(){
+  return gulp.src('./source/ts/**/**.ts')
+    .pipe(ts(tsProject))
+    .js.pipe(gulp.dest('./temp/source/js'));
 });
 
 gulp.task('default', ['lint']);
