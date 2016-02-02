789f2fb6627642a90c5185979a2c3369ad8c81a2
tocology <hwangjun7777@gmail.com>
2016-02-02 13:00:10 +0900

    feat: Add browserify

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,13 @@
+'use-strict';
+
 var gulp        = require('gulp');
 var tslint      = require('gulp-tslint');
 var ts          = require('gulp-typescript');
+var browserify  = require('browserify'),
+    source      = require('vinyl-source-stream'),
+    uglify      = require('gulp-uglify'),
+    sourcemaps  = require('gulp-sourcemaps'),
+    buffer      = require('vinyl-buffer');
 
 // options reference: https://www.npmjs.com/package/gulp-typescript
 var tsProject = ts.createProject({
@@ -24,10 +31,30 @@ gulp.task('tsc', function(){
     .js.pipe(gulp.dest('./temp/source/js'));
 });
 
-gulp.task('tsc-test', function(){
+gulp.task('tsc-tests', function(){
   return gulp.src('./test/**/**.test.ts')
     .pipe(ts(tsProject))
     .js.pipe(gulp.dest('./temp/test/'));
 });
 
-gulp.task('default', ['lint', 'tsc', 'tsc-tests']);
+gulp.task('bundle-js', function(){
+  // var browserified = transform(function(filename) {
+  //   var b = browserify({ entries: filename, debug: true });
+  //   return b.bundle();
+  // });
+  var browserified = function(filename) {
+    var b = rowserify({ entries: filename, debug: true });
+    return b.bundle();
+  }
+
+  return gulp.src('./temp/source/js/main.js')
+    .pipe(browserified)
+    .pipe(source())
+    .pipe(buffer())
+    .pipe(sourcemaps.init({ loadMaps: true }))
+    .pipe(uglify())
+    .pipe(sourcemaps.write('./'))
+    .pipe(gulp.dest('./dist/source/js/'));
+});
+
+gulp.task('default', ['lint', 'tsc', 'tsc-tests', 'bundle-js']);
