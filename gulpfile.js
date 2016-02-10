768cbe2d3982b9f5bb481eeddc83dc7ea625188d
tocology <hwangjun7777@gmail.com>
2016-02-10 19:03:27 +0900

    feat: Add more task for testing

--- gulpfile.js
+++ gulpfile.js
@@ -6,19 +6,21 @@ var path          = require('path');
 var gulp          = require('gulp');
 var tslint        = require('gulp-tslint');
 var tsc           = require('gulp-typescript');
+var browserSync   = require('browser-sync');
 var browserify    = require('browserify'),
     source        = require('vinyl-source-stream'),
     uglify        = require('gulp-uglify'),
     sourcemaps    = require('gulp-sourcemaps'),
     buffer        = require('vinyl-buffer');
 var run           = require('gulp-run');
-var
 var runSequence   = require('run-sequence');
 var karma         = require('gulp-karma');
-var browserSync   = require('browser-sync');
+var docco         = require('gulp-docco');
+var header        = require('gulp-header')
+var pkg           = require(__dirname + '/package.json');
 
 // options reference: https://www.npmjs.com/package/gulp-typescript
-var tsProject = ts.createProject({
+var tsProject = tsc.createProject({
   removeComments: true,
   noImplicitAny: true,
   target: 'ES3',
@@ -26,7 +28,7 @@ var tsProject = ts.createProject({
   declarations: false
 });
 
-var tsTestProject = ts.createProject({
+var tsTestProject = tsc.createProject({
   removeComments: true,
   noImplicitAny: true,
   target: 'ES3',
@@ -41,50 +43,94 @@ gulp.task('lint', function(){
     .pipe(tslint.report('verbose'));
 });
 
-gulp.task('tsc', function(){
-  return gulp.src('./source/ts/**/**.ts')
+gulp.task('build-source', function(){
+  return gulp.src(__dirname + "/source/*.ts")
     .pipe(tsc(tsProject))
-    .js.pipe(gulp.dest('./temp/source/js'));
+    .js.pipe(gulp.dest(__dirname + "/build/source/"));
 });
 
-gulp.task('tsc-tests', function(){
-  return gulp.src('./test/**/**.test.ts')
+gulp.task('build-test', function(){
+  return gulp.src(__dirname + "/teset/*.test.ts")
     .pipe(tsc(tsTestProject))
-    .js.pipe(gulp.dest('./temp/test/'));
+    .js.pipe(gulp.dest(__dirname + "/build/test/"));
 });
 
 // fix vinly-transform with browersify(>=9.0.0) error
-gulp.task('bundle-js', function(){
-  return browserify('./temp/source/js/main.js')
-    .bundle()
-    .pipe(source('main.js'))
+gulp.task('bundle-source', function(){
+  // return browserify('./temp/source/js/main.js')
+  //   .bundle()
+  //   .pipe(source('main.js'))
+  //   .pipe(buffer())
+  //   .pipe(sourcemaps.init({ loadMaps: true }))
+  //   .pipe(uglify())
+  //   .pipe(sourcemaps.write())
+  //   .pipe(gulp.dest('./dist/source/js'));
+  var b = browserify({
+    standalone: 'demos',
+    entries: __dirname + "/build/source/demos.js",
+    debug: true
+  });
+
+  return b.bundle()
+    .pipe(source("demos.js"))
     .pipe(buffer())
-    .pipe(sourcemaps.init({ loadMaps: true }))
-    .pipe(uglify())
-    .pipe(sourcemaps.write())
-    .pipe(gulp.dest('./dist/source/js'));
+    .pipe(gulp.dest(__dirname + "/bundled/source/"));
 });
 
 gulp.task('bundle-test', function(){
-  glob('./temp/test/**/**.test.js', function(err, filename){
-    browserify(filename)
-    .bundle()
-    .pipe(source(path.basename(filename)))
-    .pipe(gulp.dest('./dist/test/'));
-  })
+  // glob('./temp/test/**/**.test.js', function(err, filename){
+  //   browserify(filename)
+  //   .bundle()
+  //   .pipe(source(path.basename(filename)))
+  //   .pipe(gulp.dest('./dist/test/'));
+  // })
+  var b = browserify({
+    standalone: 'test',
+    entries: __dirname + "/build/test/bdd.test.js",
+    debug: true
+  });
+
+  return b.bundle()
+    .pipe(source("bdd.test.js"))
+    .pipe(buffer())
+    .pipe(gulp.dest(__dirname + "/bundled/test/"));
 });
 
-gulp.task('karma', function(cb){
-  gulp.src('./dist/test/**/**.test.js')
-    .pipe(karma({
-      configFile: 'karma.conf.js',
-      action: 'run'
-    }))
-    .on('end', cb)
-    .on('error', function(err){
-      // Make sure failed tests cause gulp to exit non-zero
-      throw err;
-    });
+gulp.task('bundle-e2e-test', function(){
+  var b = browserify({
+    standalone: 'test',
+    entries: __dirname + "/build/test/e2e.test.js",
+    debug: true
+  });
+
+  return b.bundle()
+    .pipe(source("e2e.test.js"))
+    .pipe(buffer())
+    .pipe(gulp.dest(__dirname + "/bundled/e2e-test/"));
+});
+
+gulp.task('run-unit-test', function(cb){
+  // gulp.src('./dist/test/**/**.test.js')
+  //   .pipe(karma({
+  //     configFile: 'karma.conf.js',
+  //     action: 'run'
+  //   }))
+  //   .on('end', cb)
+  //   .on('error', function(err){
+  //     // Make sure failed tests cause gulp to exit non-zero
+  //     throw err;
+  //   });
+  karma.start({
+    configFile: __dirname + "/karma.conf.js",
+    singleRun: true
+  }, cb);
+});
+
+gulp.task('run-e2e-test', function(){
+  return gulp.src('')
+    .pipe(nightwatch({
+      configFile: __dirname + '/nightwatch.json'
+    }));
 });
 
 gulp.task('build', function(cb) {
